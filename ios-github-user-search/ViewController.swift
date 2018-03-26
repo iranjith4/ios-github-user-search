d37fbf930065aa96199452451f985632e58a9e94
Ranjithkumar <r@ir4.in>
2018-03-26 19:31:46 +0530

    Initial Commit

---
+++ ios-github-user-search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-github-user-search
+//
+//  Created by ranjith on 26/03/18.
+//  Copyright © 2018 ir4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
